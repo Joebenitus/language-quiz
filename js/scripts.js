4de8910d4827e13dcc1d1152ee435fc28a7f2a5d
Joseph Karnafel <josephkarnafel@gmail.com>
2020-08-14 10:41:50 -0700

    add js constants for each question

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,16 @@
 //   Ruby
 //   C++
 //   Python
+
+$(document).ready(function(){
+  $("form#answers").submit(function(){
+    const question1 = parseInt($("input:radio[name=experience]:checked").val());
+    const question2 = parseInt($("input:radio[name=concepts]:checked").val());
+    const question3 = parseInt($("input:radio[name=subject]:checked").val());
+    const question4 = parseInt($("input:radio[name=genre]:checked").val());
+    const question5 = parseInt($("input:radio[name=vacation]:checked").val());
+
+    $("#result-python").show();
+    event.preventDefault();
+  });
+});
